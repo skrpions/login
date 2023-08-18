8660885c17d50aef8906c02d931e2e58bf3159c7
Skrpion <nestor.martinez.c@uniautonoma.edu.co>
2023-08-18 10:12:02 -0500

    refactor: dashboard responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver, private change: ChangeDetectorRef){}
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width:800px)']).subscribe((res:any) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+
+    this.change.detectChanges();
+  }
 }
